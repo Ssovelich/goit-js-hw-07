a17492d1402f42c4979d19bfbce8b557f55cd0d1
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-07-18 23:36:18 +0300

    task -5

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,28 @@
 const textInput = document.querySelector('#name-input');
 textInput.style.width = '360px'; 
 textInput.style.height = '40px';
+textInput.style.borderRadius = '4px';
+textInput.style.border = '1px solid #808080';
+textInput.style.fontFamily = 'Montserrat, sans-serif';
+textInput.style.fontWeight = '400';
+textInput.style.fontSize = '16px';
+textInput.style.lineHeight = '1.5';
+textInput.style.color = '#2e2f42';
+textInput.style.letterSpacing = '0.04em';
+textInput.style.paddingLeft = '16px'
+
 const textAll = document.querySelector('h1')
 textAll.style.fontFamily = 'Montserrat, sans-serif';
 textAll.style.fontWeight = '600';
 textAll.style.fontSize = '24px';
 textAll.style.color = '#2e2f42';
+textAll.style.lineHeight = '1.3';
+textAll.style.letterSpacing = '0.04em';
+
 const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', (event) => {
     output.textContent = event.currentTarget.value.trim() || 'Anonymous';
 })
+
+
--- js/task-4.js
+++ js/task-4.js
@@ -5,12 +5,21 @@ registerForm.style.alignItems = 'flex-start';
 registerForm.style.justifyContent = 'flex-start';
 registerForm.style.flexDirection = 'column';
 registerForm.style.gap = '16px';
-const input = document.querySelector('input');
-input.style.display = 'block';
-input.style.width = '360px';
-input.style.height = '40px';
-input.style.borderRadius = '4px';
-input.style.border = '1px solid #808080';
+const input = document.querySelectorAll('input');
+input.forEach(el => {
+    el.style.display = 'block';
+    el.style.width = '360px';
+    el.style.height = '40px';
+    el.style.borderRadius = '4px';
+    el.style.border = '1px solid #808080';
+    el.style.marginTop = "8px"
+    el.style.paddingLeft = '16px'
+    el.style.fontFamily = 'Montserrat, sans-serif';
+    el.style.fontWeight = '400';
+    el.style.fontSize = '16px';
+    el.style.lineHeight = '1.5';
+});
+
 const button = document.querySelector('button');
 button.style.display = 'block';
 button.style.color = '#ffffff';
@@ -19,6 +28,11 @@ button.style.borderRadius = '8px';
 button.style.width = '86px';
 button.style.height = '40px';
 button.style.border = 'none';
+button.style.fontFamily = '"Montserrat", sans-serif';
+button.style.lineHeight = '1.5';
+button.style.letterSpacing = '0.04em';
+button.style.fontWeight = '500';
+button.style.fontSize = '16px';
 function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
